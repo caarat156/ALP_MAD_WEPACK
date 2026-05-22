90fb7cfb1eeca1e2c12b2c867f0b1dcdb35fb8d7
caarat156 <anastasiaeugenem@gmail.com>
2026-05-22 13:39:54 +0700

    Initial Commit

---
+++ ALP_MAD_WEPACK/ALP_MAD_WEPACKApp.swift
@@ -0,0 +1,17 @@
+//
+//  ALP_MAD_WEPACKApp.swift
+//  ALP_MAD_WEPACK
+//
+//  Created by Anastasia on 22/05/26.
+//
+
+import SwiftUI
+
+@main
+struct ALP_MAD_WEPACKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ALP_MAD_WEPACK/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ALP_MAD_WEPACK
+//
+//  Created by Anastasia on 22/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
