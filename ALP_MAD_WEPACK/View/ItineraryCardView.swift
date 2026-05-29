2bb5d294b97388490307c11ba73248da3859aa77
Angelique Kyra <angeliquekw50@gmail.com>
2026-05-29 14:12:55 +0700

    trip itinerary

---
+++ ALP_MAD_WEPACK/View/ItineraryCardView.swift
@@ -0,0 +1,18 @@
+//
+//  ItineraryCardView.swift
+//  ALP_MAD_WEPACK
+//
+//  Created by MacintoshHD on 29/05/26.
+//
+
+import SwiftUI
+
+struct ItineraryCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ItineraryCardView()
+}
---
+++ ALP_MAD_WEPACK/View/ItineraryView.swift
@@ -0,0 +1,18 @@
+//
+//  ItineraryListView.swift
+//  ALP_MAD_WEPACK
+//
+//  Created by MacintoshHD on 29/05/26.
+//
+
+import SwiftUI
+
+struct ItineraryListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ItineraryListView()
+}
