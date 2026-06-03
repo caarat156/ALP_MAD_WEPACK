a28e130c2806493e60a9ad59606a928d6892a4d1
Angelique Kyra <angeliquekw50@gmail.com>
2026-06-04 02:33:32 +0700

    trip test

---
+++ ALP_MAD_WEPACK_Watch Watch AppUITests/TripViewModelTest.swift
@@ -0,0 +1,18 @@
+//
+//  TripViewModelTest.swift
+//  ALP_MAD_WEPACK_Watch Watch AppUITests
+//
+//  Created by Angelique Kyra on 04/06/26.
+//
+
+import SwiftUI
+
+struct TripViewModelTest: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TripViewModelTest()
+}
