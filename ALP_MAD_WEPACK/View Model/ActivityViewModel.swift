f88b30d0e0f811f9699c2aa422e5f8bd5d5af134
Angelique Kyra <angeliquekw50@gmail.com>
2026-06-06 14:53:27 +0700

    pisahin view model

---
+++ ALP_MAD_WEPACK/View Model/ActivityViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  ActivityViewModel.swift
+//  ALP_MAD_WEPACK
+//
+//  Created by Angelique Kyra on 06/06/26.
+//
+
+import SwiftUI
+
+struct ActivityViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ActivityViewModel()
+}
